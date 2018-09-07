e9e69f350b23f9656da0627086a1023ca42a06ba
Steinitz Emmanuel <emmanuel@botfuel.io>
2018-09-07 14:50:25 +0200

    +geoboulot

---
+++ src/dialogs/geoboulot-dialog.js
@@ -0,0 +1,5 @@
+const { BaseDialog } = require('botfuel-dialog');
+
+class Geoboulot extends BaseDialog {}
+
+module.exports = Geoboulot;
---
+++ src/views/geoboulot-view.js
@@ -0,0 +1,47 @@
+const { View, BotTextMessage } = require('botfuel-dialog');
+
+class GeoboulotView extends View {
+
+makeHtml(){
+
+  let html=`<style>
+  /* Set the size of the div element that contains the map */
+    #map {
+      height: 400px;  /* The height is 400 pixels */
+      width: 400px;  /* The width is the width of the web page */
+      }
+  </style>
+  <div id="map"></div>
+  <script>
+    // Initialize and add the map
+    function initMap() {
+    // The location of Uluru
+    var uluru = {lat: -25.344, lng: 131.036};
+    // The map, centered at Uluru
+    var map = new google.maps.Map(
+        document.getElementById('map'), {zoom: 4, center: uluru});
+    // The marker, positioned at Uluru
+    var marker = new google.maps.Marker({position: uluru, map: map});
+  }
+  </script>
+    <!--Load the API from the specified URL
+    * The async attribute allows the browser to render the page while the API loads
+    * The key parameter will contain your own API key (which is not needed for this tutorial)
+    * The callback parameter executes the initMap() function
+    -->
+  <script 
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAzHd4TV0UCFo6r7XkK4EY8gKxAfvxT-qo&callback=initMap">
+  </script>`;
+
+  return html;
+}
+
+
+  render() {
+    return [
+      new BotTextMessage(this.makeHtml()),
+    ];
+  }
+}
+
+module.exports = GeoboulotView;
